7ee2bba48dc953fe1408ced8a7b659c9d5d4dab2
huangcy <huangcy@autoai.com>
2021-08-27 18:49:00 +0800

    add protobuf.spec.ts

---
+++ src/protobuf.spec.ts
@@ -0,0 +1,26 @@
+// import pb from "testChinese_pb.js"
+
+var pb = require("./testChinese_pb.js")
+
+describe("protobuf", () => {
+
+    it("basic", () => {
+        var chi = new pb.chinese();
+        chi.setOld(10);
+        chi.setName("xiaoming");
+        // chi.setCompany("谷歌");
+        chi.setNumber(100);
+        console.log(chi.getOld());
+        console.log(chi.getName());
+        console.log(chi.getCompany());
+        // console.log(chi.getCompany_asU64());
+        console.log(chi.getNumber());
+        const bytes = chi.serializeBinary();
+
+        const result = pb.chinese.deserializeBinary(bytes);
+        console.log(result.getOld());
+        console.log(result.getName());
+        console.log(result.getCompany());
+        console.log(result.getNumber());
+    });
+})
